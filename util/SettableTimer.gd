749eada9f2a1f2d504be297d915915c1fa8310cb
SartoRiccardo <rikki.sarto@gmail.com>
2022-11-01 17:33:42 +0100

    Fixed timer being set without being in the tree

--- util/SettableTimer.gd
+++ util/SettableTimer.gd
@@ -1,7 +1,14 @@
 extends Timer
 class_name SettableTimer
 
+var added := false
+
+func _ready() -> void:
+	added = true
+
 func update_time_left(new_time_left : float) -> void:
+	if not added:
+		return
 	var cur_wait_time := wait_time
 	start(new_time_left)
 	set_wait_time(cur_wait_time)
