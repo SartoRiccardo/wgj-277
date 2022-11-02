07ae8214dfc7e9ae5d5796de59cc155a3b11455d
SartoRiccardo <rikki.sarto@gmail.com>
2022-11-02 19:12:31 +0100

    Death particles work properly

--- effects/PlantDeath.gd
+++ effects/PlantDeath.gd
@@ -1,22 +1,8 @@
 tool
 extends Particles2D
 
-export (Curve) var speed_multiplier;
-
-var elapsed := 0.0
-
 func _ready() -> void:
 	set_emitting(true)
-	preprocess = elapsed
-
-func _process(delta: float) -> void:
-	elapsed += delta
-	while elapsed > lifetime:
-		elapsed -= lifetime
-		if one_shot and not Engine.editor_hint:
-			queue_free()
-	process_material.set_shader_param("elapsed", elapsed)
-	process_material.set_shader_param("velocity_multi", speed_multiplier.interpolate(elapsed/lifetime))
 
 func set_target(global_point : Vector2) -> void:
 	process_material.set_shader_param("target", to_local(global_point))
